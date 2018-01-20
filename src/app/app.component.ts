4435d480d74fcc2964a0880f918c16dbab92d3b1
Jim Rasche <j.rasche@kunzleigh.com>
2018-01-20 15:57:08 -0500

    Added initial login prompt.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as crypto from 'crypto-js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private hashedPassword: string;
+  private encryptionKey: string;
+  public loginVerified: boolean;
+
+  public constructor() {
+    this.hashedPassword = "iZCXiEWABOCABvBcSSWHBgOkAQk=";
+    this.encryptionKey = null;
+    this.loginVerified = false;
+  }
+
+  public verifyPassword(input: string) {
+    var hash = crypto.SHA1(input);
+    var hash_Latin1 = crypto.enc.Latin1.stringify(hash);
+    var hash_Base64 = crypto.enc.Base64.stringify(hash);
+    console.log({hash_Base64, hash_Latin1, hash});
+    if (this.hashedPassword === hash_Base64) {
+      this.encryptionKey = input;
+      this.loginVerified = true;
+    }
+  }
+
 }
